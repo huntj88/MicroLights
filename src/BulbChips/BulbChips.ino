766fe905e7d3d6a322f3fb5c2abb5db1628341c1
James Hunt <huntj88@gmail.com>
2024-06-02 19:09:03 -0500

    flipped logic around for supplying power to led, experimenting with turning off

--- src/BulbChips/BulbChips.ino
+++ src/BulbChips/BulbChips.ino
@@ -1,4 +1,5 @@
 #include <util/delay.h>
+#include <avr/sleep.h>
 
 #define B1 0b0001
 
@@ -6,45 +7,62 @@ volatile bool clicked = false;
 int mode = 0;
 
 void setup() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
   DDRB = 0b0001;  // set pin 1 as output
   PUEB = 0b1110;  // pullups on input pin 2, as well as 3,4 (unused)
 
   // # Set interrupt on button press
   EIMSK |= (1 << INT0);   // Enable INT0 as interrupt vector
-
-  // this is good for normal use, but uses the clock, so should be set to low signal triggers interupt when shutting off
-  // Rising edge of INT0 generates an interrupt request
-  // EICRA = (1 << ISC01) | (1 << ISC00);
   // Falling edge of INT0 generates an interrupt request
   EICRA = (1 << ISC01) | (0 << ISC00);
   sei(); // Enable interrupts
 }
 
+int heldCounter = 0;
+
 void loop() {
-  if (clicked) {
+  bool stillPressed = !(PINB & (1<<PB2));
+  if (clicked && stillPressed) {
+    heldCounter += 1;
+    if (heldCounter > 50) {
+      turnOff();
+    }
+  }
+  
+  if (clicked && !stillPressed) {
     mode += 1;
     clicked = false;
+    heldCounter = 0;
     if (mode > 2) {
       mode = 0;
     }
   }
   if (mode == 0) {
     // max power
-    PORTB &= ~B1;   //  Set GPIO1 to LOW
+    PORTB |= B1;    //  Set GPIO1 to HIGH
     _delay_ms(40);  
   } else if (mode == 1) {
-    PORTB |= B1;    //  Set GPIO1 to HIGH
-    _delay_ms(1);   
     PORTB &= ~B1;   //  Set GPIO1 to LOW
+    _delay_ms(1);   
+    PORTB |= B1;    //  Set GPIO1 to HIGH
     _delay_ms(20);  
   } else if (mode == 2) {
-    PORTB |= B1;    //  Set GPIO1 to HIGH
-    _delay_ms(1);   
     PORTB &= ~B1;   //  Set GPIO1 to LOW
+    _delay_ms(1);   
+    PORTB |= B1;    //  Set GPIO1 to HIGH
     _delay_ms(11);  
   }
 }
 
 ISR(INT0_vect) {
-    clicked = true;
+  clicked = true;
+}
+
+void turnOff() {
+  mode = -1;
+  clicked = false;
+  heldCounter = 0;
+  PORTB &= ~B1;   //  Set GPIO1 to LOW
+  sleep_mode();
 }
