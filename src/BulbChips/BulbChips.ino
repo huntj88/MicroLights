54ebc47afee8e535ac5ecb69ec53d0cae67fa59c
James Hunt <huntj88@gmail.com>
2024-06-04 01:40:38 -0500

    better turn off/on logic

--- src/BulbChips/BulbChips.ino
+++ src/BulbChips/BulbChips.ino
@@ -3,7 +3,12 @@
 
 #define B1 0b0001
 
-volatile bool clicked = false;
+volatile bool clickStarted = false;
+volatile bool on = true;
+
+// TODO: use clock for consistent hold times, counter can be influenced by different mode delays
+volatile int heldCounter = 0;
+
 int mode = 0;
 
 void setup() {
@@ -12,57 +17,80 @@ void setup() {
   DDRB = 0b0001;  // set pin 1 as output
   PUEB = 0b1110;  // pullups on input pin 2, as well as 3,4 (unused)
 
-  // # Set interrupt on button press
-  EIMSK |= (1 << INT0);   // Enable INT0 as interrupt vector
-  // Falling edge of INT0 generates an interrupt request
-  EICRA = (1 << ISC01) | (0 << ISC00);
-  sei(); // Enable interrupts
+  PCICR = 0b00000001;      // Pin Change Interrupt Control Register set to enable PCINT Interrupt
+  PCMSK |= (1 << PCINT2);  // Select pins for interrupt, pb2 in this case.
+
+  sei();  // Enable interrupts
 }
 
-int heldCounter = 0;
-
 void loop() {
-  bool stillPressed = !(PINB & (1<<PB2));
-  if (clicked && stillPressed) {
+  if (!on) {
+    turnOff();
+  }
+
+  bool buttonCurrentlyDown = !(PINB & (1 << PB2));
+
+  // checking if held until power off
+  if (clickStarted && buttonCurrentlyDown) {
     heldCounter += 1;
     if (heldCounter > 50) {
-      turnOff();
+      on = false;
+      return;
     }
   }
-  
-  if (clicked && !stillPressed) {
+
+  // click started and released, incrementing mode
+  if (clickStarted && !buttonCurrentlyDown) {
     mode += 1;
-    clicked = false;
+    clickStarted = false;
     heldCounter = 0;
     if (mode > 2) {
       mode = 0;
     }
   }
+
   if (mode == 0) {
     // max power
-    PORTB |= B1;    //  Set GPIO1 to HIGH
-    _delay_ms(40);  
+    PORTB |= B1;  //  Set GPIO1 to HIGH
+    _delay_ms(40);
   } else if (mode == 1) {
-    PORTB &= ~B1;   //  Set GPIO1 to LOW
-    _delay_ms(1);   
-    PORTB |= B1;    //  Set GPIO1 to HIGH
-    _delay_ms(20);  
+    PORTB &= ~B1;  //  Set GPIO1 to LOW
+    _delay_ms(1);
+    PORTB |= B1;  //  Set GPIO1 to HIGH
+    _delay_ms(20);
   } else if (mode == 2) {
-    PORTB &= ~B1;   //  Set GPIO1 to LOW
-    _delay_ms(1);   
-    PORTB |= B1;    //  Set GPIO1 to HIGH
-    _delay_ms(11);  
+    PORTB &= ~B1;  //  Set GPIO1 to LOW
+    _delay_ms(1);
+    PORTB |= B1;  //  Set GPIO1 to HIGH
+    _delay_ms(11);
   }
 }
 
-ISR(INT0_vect) {
-  clicked = true;
+// invoked from pin change interupt (switch on PB2), when logic level changes
+ISR(PCINT0_vect) {
+  if (!on) {
+    return;
+  }
+
+  bool buttonCurrentlyDown = !(PINB & (1 << PB2));
+
+  if (heldCounter > 50 && !buttonCurrentlyDown) {
+    // releasing button, can trigger wake, turn back off
+    on = false;
+  } else if (!clickStarted && buttonCurrentlyDown) {
+    heldCounter = 0;
+    clickStarted = true;
+  }
 }
 
 void turnOff() {
-  mode = -1;
-  clicked = false;
-  heldCounter = 0;
-  PORTB &= ~B1;   //  Set GPIO1 to LOW
+  PORTB &= ~B1;  //  Set GPIO1 to LOW
   sleep_mode();
+
+  // will start executing here when coming out of sleep, re-init.
+  _delay_ms(100);  // prevent race case of interrupt from releasing button after powering off, and heldCounter being set to 0 then waking back up again due to the interrupt.
+  mode = -1;
+  clickStarted = false;
+  heldCounter = 0;
+  on = true;
 }
