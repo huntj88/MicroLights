84ee762d77cf3b9b27a2358742c0a0d06fc8acb9
James Hunt <huntj88@gmail.com>
2024-06-26 18:51:15 -0500

    deleted settings that were already default, separated inputLoop and shutdown, cleanup

--- src/BulbChips/BulbChips.ino
+++ src/BulbChips/BulbChips.ino
@@ -23,15 +23,10 @@ Features
 #define ClockInterruptsUntilAutoOff (AutoOffTimeSeconds / AutoOffCounterPrescale * ClockInterruptRate)
 
 volatile bool clickStarted = false;
-volatile int clockInterruptCount = 0;  // keep track of how many times the clock interrupt has happened.
+volatile int clockInterruptCount = 0;
 volatile int mode = 0;
 
-// do nothing, run inputLoop instead.
-void loop() {}
-
 void setup() {
-  setSystemClockSpeed();
-
   DDRB = 0b0001;  // set pin 1 as output
   PUEB = 0b1110;  // pullups on input pin 2 (button), as well as 3,4 (unused)
 
@@ -44,35 +39,26 @@ void setup() {
   ACSR |= (1 << ACD);  // disable Analog Comparator (power savings, module not needed, enabled by default)
 
   sei();  // Enable interrupts
-
-  inputLoop();
-}
-
-void setSystemClockSpeed() {
-  /**
- * In order to change the contents of a protected I/O register the CCP register must first be written with the correct signature.
- * After CCP is written the protected I/O registers may be written to during the next four CPU instruction cycles. All interrupts
- * are ignored during these cycles. After these cycles interrupts are automatically handled again by the CPU, and any pending
- * interrupts will be executed according to their priority.
- */
-  CCP = 0xD8;                                                              // write signature to change clock settings
-  CLKMSR = 0;                                                              // using internal 8mhz oscillator for clock
-  CCP = 0xD8;                                                              // write signature to change clock settings
-  CLKPSR = (0 << CLKPS3) | (0 << CLKPS2) | (1 << CLKPS1) | (1 << CLKPS0);  // system clock prescaler. system clock = main clock / 8
 }
 
 // clock is configured to fire the interrupt at a rate of 347 hertz
 void setUpClockCounterInterrupt() {
-  TCCR0A = 0;                                        // unset bits for clock
   OCR0A = ClockCountForInterrupt;                    // set Output Compare A value
   TCCR0B = (0 << CS02) | (0 << CS01) | (1 << CS00);  // no counter prescaling
   TCCR0B |= (1 << WGM02);                            // clear clock counter when counter reaches OCR0A value
   TIMSK0 |= (1 << OCIE0A);                           // enable Output Compare A Match clock interrupt, the interrupt is called every time the counter counts to the OCR0A value
 }
 
+void loop() {
+  // instead of invoking shutdown from inputLoop, separate them and get an empty stack to work with for each.
+  inputLoop();
+  shutdown(false);
+}
+
+// when this method returns, user is finished, and chip is now shutting down.
 void inputLoop() {
   int buttonDownCounter = 0;  // Used for detecting clicking button vs holding button by counting up or down to debounce button noise.
-  int autoOffCounter = 0;     // 16 bit ints can't count high enough, clockInterruptCount prescaled by 60
+  int autoOffCounter = 0;     // 16 bit ints can't count high enough, clockInterruptCount prescaled by 60.
   while (true) {
 
     // input loop wakes up from sleep when clock interrupt fires
@@ -94,9 +80,8 @@ void inputLoop() {
       if (buttonCurrentlyDown) {
         buttonDownCounter += 1;
         if (buttonDownCounter > 200) {
-          buttonDownCounter = 0;
           // Hold button down to shut down.
-          shutdown(false);
+          return;
         }
       } else {
         buttonDownCounter -= 10;  // Large decrement to allow any hold time to "discharge" quickly.
@@ -112,13 +97,44 @@ void inputLoop() {
         }
       }
     } else if (autoOffCounter > ClockInterruptsUntilAutoOff) {
-      autoOffCounter = 0;
-      shutdown(false);
+      // no user input auto off time elapsed, shutting down.
+      return;
     }
 
     // put CPU to into idle mode until clock interrupt sets led high/low state before releasing control back to main loop.
     set_sleep_mode(SLEEP_MODE_IDLE);
     sleep_mode();
+  }
+}
+
+void shutdown(bool wasLocked) {
+  clickStarted = false;
+
+  bool lock = checkLockSequence();
+  mode = -1;  // click started on wakeup from button interrupt, will increment to mode 0 in inputLoop.
+
+  cli();             // disable interrupts
+  PORTB &= ~LedPin;  // Set GPIO1 to LOW
+
+  TIMSK0 &= ~(1 << OCIE0A);  // disable Output Compare A Match clock interrupt
+  EIMSK |= (1 << INT0);      // Enable INT0 as interrupt vector
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sei();         // enable interrupts
+  sleep_mode();  // sleep
+
+  TIMSK0 |= (1 << OCIE0A);  // enable Output Compare A Match clock interrupt
+
+  if (lock || wasLocked) {
+    EIMSK &= ~(1 << INT0);
+    bool unlock = checkLockSequence();
+    if (unlock) {
+      mode = -1;             // click started on wakeup from button interrupt, will increment to mode 0 in inputLoop.
+      EIMSK |= (1 << INT0);  // Enable INT0 as interrupt vector
+    } else {
+      // Recursive shutdown call must be the last call to take advantage of tail recursion optimization.
+      // Without tail recursion, repeated failures of unlocking the chips would result in a stack overflow.
+      shutdown(true);
+    }
   }
 }
 
@@ -185,35 +201,6 @@ bool checkLockSequence() {
   }
 
   return completed && !canceled;
-}
-
-void shutdown(bool wasLocked) {
-  clickStarted = false;
-
-  bool lock = checkLockSequence();
-  mode = -1;  // click started on wakeup from button interrupt, will increment to mode 0 in inputLoop.
-
-  cli();             // disable interrupts
-  PORTB &= ~LedPin;  // Set GPIO1 to LOW
-
-  TIMSK0 &= ~(1 << OCIE0A);  // disable Output Compare A Match clock interrupt
-  EIMSK |= (1 << INT0);      // Enable INT0 as interrupt vector
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sei();         // enable interrupts
-  sleep_mode();  // sleep
-
-  TIMSK0 |= (1 << OCIE0A);  // enable Output Compare A Match clock interrupt
-
-  if (lock || wasLocked) {
-    EIMSK &= ~(1 << INT0);
-    bool unlock = checkLockSequence();
-    if (unlock) {
-      mode = -1;             // click started on wakeup from button interrupt, will increment to mode 0 in inputLoop.
-      EIMSK |= (1 << INT0);  // Enable INT0 as interrupt vector
-    } else {
-      shutdown(true);
-    }
-  }
 }
 
 ISR(INT0_vect) {
