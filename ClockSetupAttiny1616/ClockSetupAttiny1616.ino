deb51a95ae4b04ea4d67d49661b8b2ada5bafc9e
James Hunt <huntj88@gmail.com>
2024-08-25 00:44:53 -0500

    cleanup, represent rgb values with pwm

--- ClockSetupAttiny1616/ClockSetupAttiny1616.ino
+++ ClockSetupAttiny1616/ClockSetupAttiny1616.ino
@@ -1,13 +1,15 @@
 #include <avr/sleep.h>
 
 void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
-  CCP = CCP_IOREG_gc;
-  CLKCTRL.MCLKCTRLB |= 0x4 | CLKCTRL_PEN_bm; // div 32 main clock prescaler, clock prescaler enabled
+  // CCP = CCP_IOREG_gc;
+  // CLKCTRL.MCLKCTRLB |= 0x4 | CLKCTRL_PEN_bm;  // div 32 main clock prescaler, clock prescaler enabled
 
-  TCA0.SINGLE.PER = 0b1111111111111111; // max 16 bit uint value of 65,536
+  TCA0.SINGLE.PER = 50;  // period, count to 50 before firing interrupt
   TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
-  TCA0.SINGLE.CTRLA = 1 | 0x7; // enable counter with /1024 counter prescaling
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV16_gc | TCA_SINGLE_ENABLE_bm;  // enable counter with /1024 counter prescaling
 
   set_sleep_mode(SLEEP_MODE_IDLE);
 
@@ -18,16 +20,33 @@ void loop() {
   sleep_mode();
 }
 
-bool on = true;
 uint8_t count = 0;
+
+uint8_t rTarget = 255;
+uint8_t gTarget = 25;
+uint8_t bTarget = 25;
+
 ISR(TCA0_OVF_vect) {
-  if (count < 14) {
+
+  if (count < rTarget) {
     digitalWrite(5, HIGH);
   } else {
     digitalWrite(5, LOW);
-    count = 0;
+  }
+
+  if (count < gTarget) {
+    digitalWrite(4, HIGH);
+  } else {
+    digitalWrite(4, LOW);
+  }
+
+  if (count < bTarget) {
+    digitalWrite(3, HIGH);
+  } else {
+    digitalWrite(3, LOW);
   }
 
   count += 1;
+
   TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
 }
