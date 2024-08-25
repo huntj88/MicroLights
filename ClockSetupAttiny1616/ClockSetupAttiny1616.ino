6c1d36dd2f05b6eafdaf156ed123d99e17bd1e08
James Hunt <huntj88@gmail.com>
2024-08-25 00:54:17 -0500

    lost a tiny fraction of led color resolution in order to let the MCU go into idle for 10x as long between pwm hi/low changes

--- ClockSetupAttiny1616/ClockSetupAttiny1616.ino
+++ ClockSetupAttiny1616/ClockSetupAttiny1616.ino
@@ -7,9 +7,9 @@ void setup() {
   // CCP = CCP_IOREG_gc;
   // CLKCTRL.MCLKCTRLB |= 0x4 | CLKCTRL_PEN_bm;  // div 32 main clock prescaler, clock prescaler enabled
 
-  TCA0.SINGLE.PER = 50;  // period, count to 50 before firing interrupt
+  TCA0.SINGLE.PER = 120;  // period, count to from zero before firing interrupt
   TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
-  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV16_gc | TCA_SINGLE_ENABLE_bm;  // enable counter with /1024 counter prescaling
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc | TCA_SINGLE_ENABLE_bm;  // enable counter with /64 counter prescaling
 
   set_sleep_mode(SLEEP_MODE_IDLE);
 
@@ -28,25 +28,29 @@ uint8_t bTarget = 25;
 
 ISR(TCA0_OVF_vect) {
 
-  if (count < rTarget) {
+  if (count < rTarget / 10) {
     digitalWrite(5, HIGH);
   } else {
     digitalWrite(5, LOW);
   }
 
-  if (count < gTarget) {
+  if (count < gTarget / 10) {
     digitalWrite(4, HIGH);
   } else {
     digitalWrite(4, LOW);
   }
 
-  if (count < bTarget) {
+  if (count < bTarget / 10) {
     digitalWrite(3, HIGH);
   } else {
     digitalWrite(3, LOW);
   }
 
   count += 1;
+
+  if (count > 255 / 10) {
+    count = 0;
+  }
 
   TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
 }
