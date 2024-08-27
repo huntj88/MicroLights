635e1c5bd5c25f2a4772b20c7d8a5ea3037d29da
James Hunt <huntj88@gmail.com>
2024-08-26 21:16:03 -0500

    cleanup

--- DevBoardAttiny3226/src/main.cpp
+++ DevBoardAttiny3226/src/main.cpp
@@ -1,39 +1,21 @@
 #include <Arduino.h>
-
-// // put function declarations here:
-// int myFunction(int, int);
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   int result = myFunction(2, 3);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
-
-
 #include <avr/sleep.h>
 
 #define colorPWMFactor 8
 
-void setup() {
-  PORTC.DIRSET |= (1 << 0); // red led set mode output
-  PORTC.DIRSET |= (1 << 1); // green led set mode output
-  PORTC.DIRSET |= (1 << 2); // blue led set mode output
-  
+uint8_t rTarget = 8;
+uint8_t gTarget = 8;
+uint8_t bTarget = 120;
 
-  // pinMode(12, OUTPUT);
-  // digitalWrite(12, HIGH);
+void setup() {
+  PORTC.DIR |= (1 << 0); // red led set mode output
+  PORTC.DIR |= (1 << 1); // green led set mode output
+  PORTC.DIR |= (1 << 2); // blue led set mode output
+
   // CCP = CCP_IOREG_gc;
   // CLKCTRL.MCLKCTRLB |= 0x4 | CLKCTRL_PEN_bm;  // div 32 main clock prescaler, clock prescaler enabled
 
-  TCA0.SINGLE.PER = 120;  // period, count to from zero before firing interrupt
+  TCA0.SINGLE.PER = 2000;  // period, count to from zero before firing interrupt
   TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
   TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc | TCA_SINGLE_ENABLE_bm;  // enable counter with /64 counter prescaling
 
@@ -42,20 +24,11 @@ void setup() {
   sei();
 }
 
-uint8_t rTarget = 0;
-uint8_t gTarget = 0;
-uint8_t bTarget = 0;
-
 void loop() {
   sleep_mode();
 }
 
-// only used within the scope of TCA0_OVF_vect, volatile potentially needed later when changing led color outside the scope of interrupt?
-// uint8_t rTarget = 40;
-// uint8_t gTarget = 40;
-// uint8_t bTarget = 40;
-
-// only used within the scope of TCA0_OVF_vect, volatile not needed
+// count only used within the scope of TCA0_OVF_vect, volatile not needed
 uint8_t count = 0;
 ISR(TCA0_OVF_vect) {
   uint8_t rTargetAdjusted = rTarget / colorPWMFactor;
@@ -63,21 +36,21 @@ ISR(TCA0_OVF_vect) {
   uint8_t bTargetAdjusted = bTarget / colorPWMFactor;
 
   if (count <= rTargetAdjusted && rTargetAdjusted > 0) {
-    PORTC.OUTSET |= (1 << 0); // green led set output high
+    PORTC.OUT |= (1 << 0); // green led set output high
   } else {
-    PORTC.OUTSET &= ~(1 << 0); // green led set output high
+    PORTC.OUT &= ~(1 << 0); // green led set output high
   }
 
   if (count <= gTargetAdjusted && gTargetAdjusted > 0) {
-    PORTC.OUTSET |= (1 << 1); // green led set output high
+    PORTC.OUT |= (1 << 1); // green led set output high
   } else {
-    PORTC.OUTSET &= ~(1 << 1); // green led set output high
+    PORTC.OUT &= ~(1 << 1); // green led set output high
   }
 
   if (count <= bTargetAdjusted && bTargetAdjusted > 0) {
-    PORTC.OUTSET |= (1 << 2); // green led set output high
+    PORTC.OUT |= (1 << 2); // green led set output high
   } else {
-    PORTC.OUTSET &= ~(1 << 2); // green led set output high
+    PORTC.OUT &= ~(1 << 2); // green led set output high
   }
 
   count += 1;
