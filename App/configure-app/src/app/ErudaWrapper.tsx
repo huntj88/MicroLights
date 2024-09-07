e4bc1f1f0196352320a0eb5f5db1054244f1ca39
James Hunt <huntj88@gmail.com>
2024-09-07 17:18:16 -0500

    experimenting with side nav samples from React Pro Sidebar

--- App/configure-app/src/app/ErudaWrapper.tsx
+++ App/configure-app/src/app/ErudaWrapper.tsx
@@ -1,19 +1,10 @@
 "use client";
 
-import eruda from "eruda";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 
 export default function ErudaWrapper() {
   useEffect(() => {
-    eruda.init();
+    import("eruda").then((eruda) => eruda.default.init());
   }, []);
-
-  // TODO: post messaging should be setup in bluetooth manager/provider?
-  const onMessage = useCallback((message: any) => {
-    console.log("onMessage", message);
-  }, []);
-
-  return (
-    <></>
-  );
+  return <></>;
 }
--- App/configure-app/src/app/page.tsx
+++ App/configure-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { Button } from '@fluentui/react-components';
+import { Button } from "@fluentui/react-components";
+import { Playground } from "./nav/Playground";
 
 export default function Home() {
   return (
     <div>
       <Button>Hello world</Button>
+      <Playground />
     </div>
   );
 }
