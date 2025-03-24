8b2111a46689f762ea9bd5aa7c8e45d03c4d5cdf
James Hunt <huntj88@gmail.com>
2025-03-23 20:02:13 -0500

    script to download tinyusb

--- USBSerialSTM32/setup.sh
+++ USBSerialSTM32/setup.sh
@@ -1,7 +1,15 @@
-python3 tinyusb/tools/get_deps.py stm32c0 &&
-
 cd libs &&
+
 rm -rf lwjson &&
 curl -L https://github.com/MaJerle/lwjson/archive/refs/tags/v1.7.0.tar.gz > lwjson.tar.gz &&
 tar -xzf lwjson.tar.gz &&
-rm lwjson.tar.gz
+rm lwjson.tar.gz &&
+mv lwjson-1.7.0 lwjson &&
+
+rm -rf tinyusb &&
+curl -L https://github.com/hathach/tinyusb/archive/refs/tags/0.18.0.tar.gz > tinyusb.tar.gz &&
+tar -xzf tinyusb.tar.gz &&
+rm tinyusb.tar.gz &&
+mv tinyusb-0.18.0 tinyusb &&
+
+python3 tinyusb-0.18.0/tools/get_deps.py stm32c0
