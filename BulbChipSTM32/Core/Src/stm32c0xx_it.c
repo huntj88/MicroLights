e8a68be21422a6715e046352e6a6d08d493f9439
James Hunt <huntj88@gmail.com>
2025-06-29 10:46:53 -0500

    basic interrupt on timer2 for bulb modes

--- BulbChipSTM32/Core/Src/stm32c0xx_it.c
+++ BulbChipSTM32/Core/Src/stm32c0xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim2;
 extern PCD_HandleTypeDef hpcd_USB_DRD_FS;
 /* USER CODE BEGIN EV */
 
@@ -84,7 +85,7 @@ void NMI_Handler(void)
 //void HardFault_Handler(void)
 //{
 //  /* USER CODE BEGIN HardFault_IRQn 0 */
-////
+//
 //  /* USER CODE END HardFault_IRQn 0 */
 //  while (1)
 //  {
@@ -125,11 +126,11 @@ void PendSV_Handler(void)
 //void SysTick_Handler(void)
 //{
 //  /* USER CODE BEGIN SysTick_IRQn 0 */
-////
+//
 //  /* USER CODE END SysTick_IRQn 0 */
 //  HAL_IncTick();
 //  /* USER CODE BEGIN SysTick_IRQn 1 */
-////
+//
 //  /* USER CODE END SysTick_IRQn 1 */
 //}
 
@@ -146,13 +147,29 @@ void PendSV_Handler(void)
 //void USB_DRD_FS_IRQHandler(void)
 //{
 //  /* USER CODE BEGIN USB_DRD_FS_IRQn 0 */
-////
+//
 //  /* USER CODE END USB_DRD_FS_IRQn 0 */
 //  HAL_PCD_IRQHandler(&hpcd_USB_DRD_FS);
 //  /* USER CODE BEGIN USB_DRD_FS_IRQn 1 */
-////
+//
 //  /* USER CODE END USB_DRD_FS_IRQn 1 */
 //}
+
+/**
+  * @brief This function handles TIM2 global interrupt.
+  */
+void TIM2_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+
+  /* USER CODE END TIM2_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
+
+  // TODO: handle bulb chip modes
+
+  /* USER CODE END TIM2_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
