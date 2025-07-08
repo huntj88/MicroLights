44498630c3fa93a041cf0a495006d91ba84258d0
James Hunt <huntj88@gmail.com>
2025-07-08 18:34:41 -0500

    create new empty project for STM32C071FBPx, as well as empty lib project for shared code

---
+++ BulbChipSTM32Common/src/include/test.h
@@ -0,0 +1,13 @@
+/*
+ * test.h
+ *
+ *  Created on: Jul 8, 2025
+ *      Author: jameshunt
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+void test();
+
+#endif /* TEST_H_ */
---
+++ BulbChipSTM32Common/src/test.c
@@ -0,0 +1,12 @@
+/*
+ * test.c
+ *
+ *  Created on: Jul 8, 2025
+ *      Author: jameshunt
+ */
+
+#include "include/test.h"
+
+void test() {
+
+}
