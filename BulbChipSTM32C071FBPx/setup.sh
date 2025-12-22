9f787d49b70d1ecade78cd0667b2680249af036f
James Hunt <huntj88@gmail.com>
2025-12-21 19:30:18 -0600

    pin lib dependencies to commits

--- BulbChipSTM32C071FBPx/setup.sh
+++ BulbChipSTM32C071FBPx/setup.sh
@@ -1,23 +1,38 @@
+# lwjson
+LWJSON_VERSION="v1.7.0"
+LWJSON_COMMIT="278848a551674d73c9e3b1045fd32c4e455a6314"
+
+# tinyusb
+TINYUSB_VERSION="0.18.0"
+TINYUSB_COMMIT="86ad6e56c1700e85f1c5678607a762cfe3aa2f47"
+
+# Unity
+UNITY_VERSION="v2.6.1"
+UNITY_COMMIT="cbcd08fa7de711053a3deec6339ee89cad5d2697"
+
 mkdir -p libs &&
 cd libs &&
 
 rm -rf lwjson &&
-curl -L https://github.com/MaJerle/lwjson/archive/refs/tags/v1.7.0.tar.gz > lwjson.tar.gz &&
+echo "Downloading lwjson $LWJSON_VERSION ($LWJSON_COMMIT)..." &&
+curl -L "https://github.com/MaJerle/lwjson/archive/$LWJSON_COMMIT.tar.gz" > lwjson.tar.gz &&
 tar -xzf lwjson.tar.gz &&
 rm lwjson.tar.gz &&
-mv lwjson-1.7.0 lwjson &&
+mv "lwjson-$LWJSON_COMMIT" lwjson &&
 
 rm -rf tinyusb &&
-curl -L https://github.com/hathach/tinyusb/archive/refs/tags/0.18.0.tar.gz > tinyusb.tar.gz &&
+echo "Downloading tinyusb $TINYUSB_VERSION ($TINYUSB_COMMIT)..." &&
+curl -L "https://github.com/hathach/tinyusb/archive/$TINYUSB_COMMIT.tar.gz" > tinyusb.tar.gz &&
 tar -xzf tinyusb.tar.gz &&
 rm tinyusb.tar.gz &&
-mv tinyusb-0.18.0 tinyusb &&
+mv "tinyusb-$TINYUSB_COMMIT" tinyusb &&
 
 python3 tinyusb/tools/get_deps.py stm32c0 &&
 
 rm -rf Unity &&
-curl -L https://github.com/ThrowTheSwitch/Unity/archive/refs/tags/v2.6.1.tar.gz > Unity.tar.gz &&
+echo "Downloading Unity $UNITY_VERSION ($UNITY_COMMIT)..." &&
+curl -L "https://github.com/ThrowTheSwitch/Unity/archive/$UNITY_COMMIT.tar.gz" > Unity.tar.gz &&
 tar -xzf Unity.tar.gz &&
 rm Unity.tar.gz &&
-mv Unity-2.6.1 Unity
+mv "Unity-$UNITY_COMMIT" Unity
 
