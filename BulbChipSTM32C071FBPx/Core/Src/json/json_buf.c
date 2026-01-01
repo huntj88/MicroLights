afbfc631d303ba8c5a15766cb50e9afa0ea62b89
James Hunt <huntj88@gmail.com>
2026-01-01 14:55:12 -0600

    comment

--- BulbChipSTM32C071FBPx/Core/Src/json/json_buf.c
+++ BulbChipSTM32C071FBPx/Core/Src/json/json_buf.c
@@ -7,4 +7,7 @@
 
 #include "json/json_buf.h"
 
+/* Global buffer for reading JSON data.
+ * Note: This variable is not thread-safe and is intended for single-threaded use only.
+ */
 uint8_t jsonBuf[PAGE_SECTOR];
